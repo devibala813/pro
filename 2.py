d017a4ca96cc616bf7e34cf6e7223e0c4bb4bbd8
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-11 22:39:29 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,12 +2,12 @@ d1,d2=input().strip().split(" ")
 d2=int(d2)
 i=0;
 while i<len(d1)-1 and d2:
-   if(d1[i]>d1[j+1]):
+   if(d1[i]>d1[i+1]):
       d2=-1
       d1=d1[:i]+d1[i+1:]
       if(i!=0):
          j=-1
-    else:
+   else:
        j+=1
 d=d1[:len(d1)-d2]
 print(d)
