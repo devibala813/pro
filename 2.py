a7f526b427dafc1da818cd37d5eb4d5a620ffa67
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-11 22:45:49 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,10 +3,10 @@ d2=int(d2)
 i=0;
 while i<len(d1)-1 and d2:
    if(d1[i]>d1[i+1]):
-      d2=-1
+      d2-=1
       d1=d1[:i]+d1[i+1:]
       if(i!=0):
-         i=-1
+         i-=1
    else:
        i+=1
 d=d1[:len(d1)-d2]
