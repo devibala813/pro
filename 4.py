550e1428bdc993f2ea10abb4abf52331f69b43f3
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-11 23:04:17 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -4,7 +4,7 @@ if len(w1)>len(w2):
    w1,w2=w2,w1
 s=0
 while s<len(w1):
-   x+=(ord(w1[s])-ord(w2[s]))
+   x+=(ord(w2[s])-ord(w1[s]))
    s+=1
 for s in range(s,len(w2)):
    x+=ord(w2[s])-ord('a')+1
