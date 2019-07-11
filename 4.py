9bfb76b42bb86c5c1f252ca45cb0c75a94391003
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-11 23:02:30 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+w1,w2=map(str,input().split())
+x=0
+if len(w1)>len(w2):
+   w1,w2=w2,w1
+s=0
+while s<len(w1):
+   x+=(ord(w1[s])-ord(w2[s]))
+   s+=1
+for s in range(s,len(w2)):
+   x+=ord(w2[s])-ord('a')+1
+print(x)
